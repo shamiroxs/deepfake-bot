e41e94f71548ffd2205a2d62c7df18aae825e345
Shamir Asharf <shamirkolakkadan26@gmail.com>
2025-03-17 17:57:28 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,34 +2,35 @@ import multiprocessing
 import subprocess
 import time
 
-start_time = time.time()
-
-print("Running frame.py...\n")
-subprocess.run(["python", "frame.py"])
-
 def run_classify():
     subprocess.run(["python", "classify.py"])
 
 def run_bgclassify():
     subprocess.run(["python", "bgclassify.py"])
 
-print("Starting parallel processes: classify.py & bgclassify.py\n")
+if __name__ == "__main__":
+    start_time = time.time()
 
-multiprocessing.set_start_method('spawn', force=True)  # Ensure safe multiprocessing
+    print("Running frame.py..")
+    subprocess.run(["python", "frame.py"])
 
-process1 = multiprocessing.Process(target=run_classify)
-process2 = multiprocessing.Process(target=run_bgclassify)
+    print("Starting parallel processes: classify.py & bgclassify.py\n")
 
-process1.start()
-process2.start()
+    multiprocessing.set_start_method('spawn', force=True)
 
-process1.join()
-process2.join()
+    process1 = multiprocessing.Process(target=run_classify)
+    process2 = multiprocessing.Process(target=run_bgclassify)
 
-print("Running result.py...\n")
-subprocess.run(["python", "combine.py"])
-subprocess.run(["python", "result.py"])
+    process1.start()
+    process2.start()
 
-end_time = time.time()
-print("Execution completed.")
-print(f"Overall execution time: {end_time - start_time:.2f} seconds")
+    process1.join()
+    process2.join()
+
+    print("Running result.py...\n")
+    subprocess.run(["python", "combine.py"])
+    subprocess.run(["python", "result.py"])
+
+    end_time = time.time()
+    print("Execution completed.")
+    print(f"Overall execution time: {end_time - start_time:.2f} seconds")
